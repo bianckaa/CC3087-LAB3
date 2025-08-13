14d2f6a9c7be31a995565962898fb53425120f6a
bianckaa <bianckaraxon@gmail.com>
2025-08-12 22:54:07 -0600

    finals changes

--- app/src/main/java/com/example/laboratorio_3/MainActivity.kt
+++ app/src/main/java/com/example/laboratorio_3/MainActivity.kt
@@ -16,9 +16,13 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -36,6 +40,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -58,11 +63,9 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ListaTareasScreen() {
-    LocalContext.current
+    val context = LocalContext.current
     var nuevaTarea by remember { mutableStateOf("") }
     val tareas = remember { mutableStateListOf<String>() }
-
-    stringResource(R.string.tarea_vacia)
 
     Column(
         modifier = Modifier
@@ -73,74 +76,96 @@ fun ListaTareasScreen() {
     ) {
         Text(
             stringResource(R.string.titulos),
-            modifier = Modifier.padding(top = 16.dp, start = 90.dp),
+            modifier = Modifier.padding(top = 10.dp, start = 110.dp),
             fontWeight = FontWeight.Bold,
-            fontSize = 24.sp,
+            fontSize = 30.sp,
+            color = colorResource(id = R.color.navy)
         )
+
         LazyColumn(
             modifier = Modifier
-                .fillMaxWidth()
+                .width(300.dp)
                 .weight(1f),
-            horizontalAlignment = Alignment.CenterHorizontally
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = if (tareas.isEmpty()) Arrangement.Top else Arrangement.Top
         ) {
             if (tareas.isEmpty()) {
                 item {
                     Text(
                         text = stringResource(R.string.lista_vacia),
-                        modifier = Modifier
-                            .padding(top = 58.dp),
-                        color = colorResource(id = R.color.text_blue),
-                        fontWeight = FontWeight.SemiBold,
+                        modifier = Modifier.padding(top = 58.dp),
+                        color = colorResource(id = R.color.navy),
+                        fontWeight = FontWeight.W300,
                     )
                 }
             } else {
-                items(tareas) { tarea ->
+                itemsIndexed(tareas) { index, tarea ->
                     Text(
                         text = tarea,
-                        color = colorResource(id = R.color.text_blue),
-                        fontWeight = FontWeight.SemiBold,
+                        color = colorResource(id = R.color.navy),
+                        fontWeight = FontWeight.W300,
+                        fontSize = 16.sp,
                         modifier = Modifier
                             .fillMaxWidth()
-                            .padding(vertical = 8.dp, horizontal = 8.dp)
+                            .padding(
+                                top = if (index == 0) 58.dp else 8.dp,
+                                bottom = 8.dp,
+                                start = 4.dp,
+                                end = 8.dp
+                            ),
+                        textAlign = TextAlign.Start
                     )
                 }
             }
-
         }
 
         TextField(
             value = nuevaTarea,
             onValueChange = { nuevaTarea = it },
-            label = { Text(stringResource(R.string.agregar_tarea)) },
+            placeholder = { Text(stringResource(R.string.agregar_tarea)) },
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Filled.Edit,
+                    contentDescription = ""
+                )
+            },
             modifier = Modifier.width(300.dp),
-            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done)
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
         )
 
         Spacer(modifier = Modifier.height(8.dp))
 
-
         Button(
             onClick = {
                 if (nuevaTarea.isBlank()) {
-                    Toast.makeText(currentContext,
-                        mensajeErrorTareaVacia,
-                        Toast.LENGTH_SHORT)
-                        .show
+                    Toast.makeText(
+                        context,
+                        context.getString(R.string.error_tarea_vacia),
+                        Toast.LENGTH_SHORT
+                    ).show()
                 } else {
-                    tareas.add(nuevaTarea)
+                    tareas.add("✰ ${nuevaTarea.trim()}")
                     nuevaTarea = ""
                 }
             },
             modifier = Modifier
                 .width(200.dp)
-                .padding(bottom = 50.dp)
+                .height(90.dp)
+                .padding(bottom = 50.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = colorResource(id = R.color.teal),
+                contentColor = colorResource(id = R.color.beige)
+            )
         ) {
-            Text(stringResource(R.string.agregar_tarea),
+            Text(
+                stringResource(R.string.agregar_tarea),
                 fontWeight = FontWeight.Bold,
-                fontSize = 16.sp)
+                fontSize = 16.sp
+            )
         }
     }
 }
+
 
 @Composable
 fun Background() {
